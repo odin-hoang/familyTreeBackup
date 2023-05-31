98588bd6a3eb42b360cc712e72e6f85bf23a6575
lương vĩnh hưng <vinhhungalex03@gmail.com>
2023-05-31 20:19:23 +0700

    update database
    change some things

--- databaseQuery/FTM_USER.sql
+++ databaseQuery/FTM_USER.sql
@@ -3,18 +3,11 @@ use FTM_USER
 
 create table UserData
 (
-	UserName varchar(20) not null primary key,
+
 	FirstName varchar(20),
 	LastName varchar(40),
-	Email varchar(50),
+	Email varchar(50) not null primary key,
 	UserPassword varchar(50) not null,
 	UserProfilePicture varbinary(max)
 )
 
-insert into UserData(UserName,FirstName,LastName,Email,UserPassword,UserProfilePicture) values ('HungLuong','Hung','Luong vinh','vinhhung@gmail.com','88888888',null)
-insert into UserData(UserName,FirstName,LastName,Email,UserPassword,UserProfilePicture) values ('ad','Hung1','Luong vinh','vinhhung@gmail.com','88888888',null)
-
-
-
-select * 
-from UserData
