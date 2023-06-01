9a0b89203cae95c60e1783142213ba769bec62c4
uKaxg <uka.pgr@gmail.com>
2023-06-01 10:30:11 +0700

    Revert "Merge pull request #20 from uKaxG/database"
    This reverts commit 4968d1ae441ce82bafd1ddb7701137b18e016d61, reversing
    changes made to e3fe8b9a2646f06da0c2f673c094941e0698523f.

--- databaseQuery/FTM.sql
+++ databaseQuery/FTM.sql
@@ -3,10 +3,10 @@ use FTM
 
 create table MEMBER
 (
-	MemberID int not null IDENTITY(1,1) primary key,
+	MemberID int not null primary key,
 	FirstName varchar(20) not null,
 	LastName varchar(40) not null,
-	DateOfBirth date not null, 
+	DateOfBirt date not null, 
 	Gender varchar(3) not null,
 	PlaceOfOrigin varchar(255),
 	Email varchar(50),
@@ -19,7 +19,7 @@ create table RELATIONSHIP_SPOUSE
 (
 	MemberID1 int,
 	MemberID2 int,
-	SpouseID int not null IDENTITY(1,1) primary key,
+	SpouseID int not null primary key,
 	constraint FK_Relationship_Spouse_Member1 foreign key (MemberID1) references Member (MemberID),
 	constraint FK_Relationship_Spouse_Member2 foreign key (MemberID2) references Member (MemberID)
 )
